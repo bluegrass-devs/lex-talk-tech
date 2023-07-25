408c49e6880df539d6dca4006cfdb8192ea1ee14
Nealium104 <neal.grindstaff@gmail.com>
2023-07-25 01:02:48 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: [
+            "placekitten.com"
+        ]
+    }
+
+}
+
+module.exports = nextConfig
